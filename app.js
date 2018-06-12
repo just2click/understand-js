28eee3719549ab68df60cec929d4b2464f98b0db
just2click <just2click@outlook.com>
2018-06-12 14:20:33 +0300

    Sampling scope issues, where a variable is defined

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-var a;
-console.log(a);
-
-if (a === undefined) {
-    console.log('a is undefined');
-} else {
-    console.log('a is defined');
+function b() {
+    var myVar;
+    console.log();
 }
+
+function a() {
+    var myVar = 2;
+    console.log(myVar);
+    b();
+}
+
+var myVar = 1;
+console.log(myVar);
+a();
