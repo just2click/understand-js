7b2dd4e31921d1fb582ac302a2c2e86116119e19
just2click <just2click@outlook.com>
2018-06-14 15:11:57 +0300

    Show how functions can have properties

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
+function greet() {
+    console.log('hi');
+}
 
-var person = new Object();
+// Since in javascript a function is an object we can add properties to it
+greet.language = 'English';
 
-person["firstname"] = "Dror";
-person["lastname"] = "Avidov";
-
-var firstNameProp = 'firstname';
-console.log(person);
-console.log(person[firstNameProp]);
-
-console.log(person.lastname);
-
-person.address = new Object();
-person.address.street = "9 Yael Rom st.";
-person.address.city = "Petah Tikva";
-
-console.log(person["address"]["city"]);
+console.log(greet); // Shows the function
+console.log(greet.language);    // Return the proprty
+console.log(greet.code);
