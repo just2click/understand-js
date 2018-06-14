279597af3396af8d998c6ef8518897d261411814
just2click <just2click@outlook.com>
2018-06-14 12:09:23 +0300

    new Object() and computed access notation []

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-// We get the second lib name as the two names are equal, thus they "run" over each other
-console.log(libraryName);
+
+var person = new Object();
+
+person["firstname"] = "Dror";
+person["lastname"] = "Avidov";
+
+var firstNameProp = 'firstname';
+console.log(person);
+console.log(person[firstNameProp]);
+
+console.log(person.lastname);
+
+person.address = new Object();
+person.address.street = "9 Yael Rom st.";
+person.address.city = "Petah Tikva";
+
+console.log(person["address"]["city"]);
--- lib2.js
+++
@@ -1,2 +0,0 @@
-// One way to resolve this is, check if the global variable name is already in use before changing it
-window.libraryName = window.libraryName || "Lib 2";
