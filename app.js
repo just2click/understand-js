8b8e14466b97eec185664260c7f922ac296f75c5
just2click <just2click@outlook.com>
2018-06-18 11:03:33 +0300

    Handling argumants

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-// An array can have all kinds of object types, including functions which are basically objects
-var arr = [
-    1,
-    false,
-    {
-        name: 'Dror',
-        address: 'Yael Rom st. 9'
-    },
-    function (name) {
-        var greetings = "Hello ";
-        
-        console.log(greetings + name);
-    },
-    'Hello'
-];
+function greet(firstName, lastName, language) {
+    
+    // Default language if missing
+    language = language || 'en';
+    
+    // If no params were sent return
+    if (arguments.length === 0) {
+        console.log('No argumanets');
+        console.log('--------------')
+    }
+    
+    console.log(firstName);
+    console.log(lastName);
+    console.log(language);
+    console.log(arguments);
+    console.log('argumnets[0]: ' + arguments[0]);
+    console.log('--------------')
+}
 
-console.log(arr);
-arr[3](arr[2].name);
+greet();
+greet('Jhon');
+greet('Jhon', 'Doe');
+greet('Jhon', 'Doe', 'es');
+greet('Jhon', 'Doe', 'es', 'Main st 222', 'Ney York');
