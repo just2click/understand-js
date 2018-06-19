9475bb72239403079c147197003a4d9d97203a72
just2click <just2click@outlook.com>
2018-06-19 15:01:10 +0300

    Prototypial inheritance

--- app.js
+++ app.js
@@ -1,67 +1,29 @@
-// Functional programming
-function mapForEach(arr, fn) {
-    var newArr = [];
-
-    for (var i = 0; i < arr.length; i++) {
-        newArr.push(fn(arr[i]))   
+var person = {
+    firstName: 'Default',
+    lastName: 'Default',
+    
+    getFullName: function () {
+        return this.firstName + ' ' + this.lastName;
     }
-    
-    return newArr;
-    
 }
 
-var arr1 = [1, 2, 3];
-console.log(arr1);
-
-var arr2 = [];
-for (var i = 0; i < arr1.length; i++) {
-    arr2.push(arr1[i] * 2);
+var john = {
+    firstName: 'John',
+    lastName: 'Doe'
 }
 
-console.log(arr2);
+// Don't do that EVER, for demo purposes only!!!
+john.__proto__ = person;
 
-// Using mapForEach
-var arr3 = mapForEach(arr1, function (item) {
-   return item * 2; 
-});
-    
-console.log(arr3);
+console.log(john.getFullName());    // Go's to the proto
+console.log(john.firstName);    // Prints John and not defualt since it findes the john object property first
 
-// Using mapForEach with a different action, not a mathemathical operation
-var arr4 = mapForEach(arr1, function (item) {
-   return item > 2; 
-});
-    
-console.log(arr4);
-
-var checkPastLimit = function (limiter, item) {
-    return item > limiter;
+var jane = {
+    firstName: 'Jane',
 }
 
-var arr5 = mapForEach(arr1, checkPastLimit.bind(this, 1));
+jane.__proto__ = person;
 
-console.log('binded: ', arr5);
+console.log(jane.getFullName());    // Go's to the proto
+console.log(jane.firstName);    // Prints John and not defualt since it findes the john object property first
 
-// Exercise
-var checkLimiter = function (limiter) {
-    return checkPastLimit.bind(this, limiter);
-}
-
-var arr6 = mapForEach(arr1, checkLimiter(3));
-
-console.log('wrapped: ', arr6);
-
-
-//-----------------------------//
-// Functional programming with underscore
-var arr7 = _.map(arr1, function(item) {
-    return item * 4;
-});
-
-console.log('With underscore: ', arr7);
-
-var arr8 = _.filter([1, 2, 3, 4, 5, 6, 7], function (item) {
-    return item % 2 === 0;
-});
-
-console.log('With underscore filter: ', arr8);
