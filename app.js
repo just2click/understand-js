49c90299c921c58e5afac694b65c82a6f2f47830
just2click <just2click@outlook.com>
2018-06-21 13:24:29 +0300

    Adding methods with prototypes for functions

--- app.js
+++ app.js
@@ -1,40 +1,54 @@
-var person = {
-    firstName: 'Default',
-    lastName: 'Default',
+function Person () {
+
+    // The actual object (which is empty)
+    console.log(this);
     
-    getFullName: function () {
-        return this.firstName + ' ' + this.lastName;
-    }
-}
-
-var john = {
-    firstName: 'John',
-    lastName: 'Doe'
-}
-
-// Don't do this EVER!! for demo onlu
-john.__proto__ = person;
-
-for (var prop in john) {
-    if (john.hasOwnProperty(prop)) {
-        console.log(prop + ': ' + john[prop]);
-    }
-}
-var jane = {
-    address: '111 Main st',
+    this.firstName = 'John';
+    this.lastName = 'Doe';
+    // Show that this function is invoked after the new operator
+    console.log('Person is invoked');
     
-    getFormalFullName: function () {
-        return this.lastName + ', ' + this.firstName;
-    }
+    // If we return an object from this function
+    // we actually override the above object props
+    //return { gretting: 'Opps' };
 }
 
-var jim = {
+Person2.prototype.getFullName = function () {
+    return this.firstName + ' ' + this.lastName;
+}
+
+
+// Setting the methods this way is very efficient memory wise
+// as the methods are only created once on the prototype and
+// are not duplicated with every object instance
+Person2.prototype.getFormalFullName = function () {
+    return this.lastName + ', ' + this.firstName;
+}
+
+// A function constructor with params
+function Person2 (firtsName, lastName) {
+
+    // The actual object (which is empty)
+    console.log(this);
     
-    getFirstName: function() {
-        return this.getFirstName;
-    }
+    this.firstName = firtsName;
+    this.lastName = lastName;
+    // Show that this function is invoked after the new operator
+    console.log('Person2 is invoked');
 }
 
-_.extend(john, jane, jim);
+var john = new Person();
 
-console.log(john.getFormalFullName());
+console.log(john);
+    
+var john2 = new Person2('John', 'Doe');
+
+console.log(john2);
+
+console.log('Get fullname: ', john2.getFullName());
+
+var jane = new Person2('Jane', 'Doe');
+
+console.log(jane);
+
+console.log('Get formal fullname: ', jane.getFormalFullName());
