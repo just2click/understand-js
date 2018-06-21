858b55eb1710a9dea8c39f93dc54bcd8f631db4b
just2click <just2click@outlook.com>
2018-06-21 11:33:27 +0300

    Extend objects with underscore

--- app.js
+++ app.js
@@ -1,3 +1,40 @@
-var a = {};
-var f = function () {};
-var c = [];
+var person = {
+    firstName: 'Default',
+    lastName: 'Default',
+    
+    getFullName: function () {
+        return this.firstName + ' ' + this.lastName;
+    }
+}
+
+var john = {
+    firstName: 'John',
+    lastName: 'Doe'
+}
+
+// Don't do this EVER!! for demo onlu
+john.__proto__ = person;
+
+for (var prop in john) {
+    if (john.hasOwnProperty(prop)) {
+        console.log(prop + ': ' + john[prop]);
+    }
+}
+var jane = {
+    address: '111 Main st',
+    
+    getFormalFullName: function () {
+        return this.lastName + ', ' + this.firstName;
+    }
+}
+
+var jim = {
+    
+    getFirstName: function() {
+        return this.getFirstName;
+    }
+}
+
+_.extend(john, jane, jim);
+
+console.log(john.getFormalFullName());
