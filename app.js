ac215fa6e82bb52021ac06069f9c9beb8d57e9d4
just2click <just2click@outlook.com>
2018-06-21 14:13:17 +0300

    Using object create

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-// Adding custom feature to Array prototype can be problemait
-// as it may colide with exusting libraries
-
-Array.prototype.myCustomFeatre = 'cool!';
-
-var arr = ['John', 'Jane', 'Jim'];
-
-for (var prop in arr) {
-    console.log(prop + ': ' + arr[prop]);
+var person = {
+    firstName: 'Default',
+    lastName: 'Default',
+    
+    greet: function () {
+        return 'Hi ' + this.firstName;
+    }
 }
 
-// For arries used a standart for loop
+var john = Object.create(person);
+
+// An empty ibject based on person is created
+console.log(john);
+
+// In order to have our data we need to populate it
+john.firstName = 'John';
+john.lastName = 'Doe';
+
+console.log(john);
+
+console.log(john.greet());
