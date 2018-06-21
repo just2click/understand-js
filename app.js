eac978dccef06d9c1d2244b92a7d681f7f8d70ee
just2click <just2click@outlook.com>
2018-06-21 09:48:34 +0300

    Show the in the basis of an Array, an Object and a Function there's always an object

--- app.js
+++ app.js
@@ -1,29 +1,3 @@
-var person = {
-    firstName: 'Default',
-    lastName: 'Default',
-    
-    getFullName: function () {
-        return this.firstName + ' ' + this.lastName;
-    }
-}
-
-var john = {
-    firstName: 'John',
-    lastName: 'Doe'
-}
-
-// Don't do that EVER, for demo purposes only!!!
-john.__proto__ = person;
-
-console.log(john.getFullName());    // Go's to the proto
-console.log(john.firstName);    // Prints John and not defualt since it findes the john object property first
-
-var jane = {
-    firstName: 'Jane',
-}
-
-jane.__proto__ = person;
-
-console.log(jane.getFullName());    // Go's to the proto
-console.log(jane.firstName);    // Prints John and not defualt since it findes the john object property first
-
+var a = {};
+var f = function () {};
+var c = [];
