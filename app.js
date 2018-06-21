4824eb16865507a031e151e1ff9322655750c5e0
just2click <just2click@outlook.com>
2018-06-21 13:51:39 +0300

    Samples of functionality added to String and Number using th prototype

--- app.js
+++ app.js
@@ -1,54 +1,11 @@
-function Person () {
-
-    // The actual object (which is empty)
-    console.log(this);
-    
-    this.firstName = 'John';
-    this.lastName = 'Doe';
-    // Show that this function is invoked after the new operator
-    console.log('Person is invoked');
-    
-    // If we return an object from this function
-    // we actually override the above object props
-    //return { gretting: 'Opps' };
+String.prototype.isLengthGreaterThan = function (limit) {
+    return this.length > limit;
 }
 
-Person2.prototype.getFullName = function () {
-    return this.firstName + ' ' + this.lastName;
+console.log("Dror Avidov".isLengthGreaterThan(8));
+
+Number.prototype.isPositive = function () {
+    return this > 0;
 }
 
-
-// Setting the methods this way is very efficient memory wise
-// as the methods are only created once on the prototype and
-// are not duplicated with every object instance
-Person2.prototype.getFormalFullName = function () {
-    return this.lastName + ', ' + this.firstName;
-}
-
-// A function constructor with params
-function Person2 (firtsName, lastName) {
-
-    // The actual object (which is empty)
-    console.log(this);
-    
-    this.firstName = firtsName;
-    this.lastName = lastName;
-    // Show that this function is invoked after the new operator
-    console.log('Person2 is invoked');
-}
-
-var john = new Person();
-
-console.log(john);
-    
-var john2 = new Person2('John', 'Doe');
-
-console.log(john2);
-
-console.log('Get fullname: ', john2.getFullName());
-
-var jane = new Person2('Jane', 'Doe');
-
-console.log(jane);
-
-console.log('Get formal fullname: ', jane.getFormalFullName());
+console.log(Number(-1).isPositive());
