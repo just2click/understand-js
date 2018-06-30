b39fa852d6e9584932c7f16e4bafebff44a39766
just2click <just2click@outlook.com>
2018-06-30 20:14:56 +0300

    Add sample polyfeel

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+// Polyfeel
+if (!Object.create) {
+    Object.create = function (o) {
+        if (arguments.length > 1) {
+            throw new Error('Object.create implementation'
+            + ' only accepts the first parameter');
+        }
+        
+        function F() {}
+        F.prototype = o;
+        return new F();
+    }
+};
+
 var person = {
     firstName: 'Default',
     lastName: 'Default',
